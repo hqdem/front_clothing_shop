07fdbf7902a5cf09864103f98f3811adbd1bd60c
hqdem <ilyadem4@gmail.com>
2023-02-11 13:02:58 +0300

    add loader spinner component

---
+++ src/components/LoaderSpinner/LoaderSpinner.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from "./loader-spinner.module.css"
+
+const LoaderSpinner = () => {
+    return (
+        <span className={classes.loader}></span>
+    )
+}
+
+export default LoaderSpinner
