d3963a0dccebadefc8b2ec17adbd3ccac4705e36
hqdem <ilyadem4@gmail.com>
2023-02-12 14:05:48 +0300

    add animations to the project

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import 'animate.css'
 import {QueryClient, QueryClientProvider} from "react-query"
 import {BrowserRouter} from "react-router-dom"
 
