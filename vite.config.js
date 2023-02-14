92c2251ef60ac3df2cd021c976df7221fb3e9b0b
hqdem <ilyadem4@gmail.com>
2023-02-14 22:56:09 +0300

    change base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/clothing_shop_front/'
+  base: '/front_clothing_shop/'
 })
