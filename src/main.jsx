631151f63307c2894b25df88f802f82913a4a776
hqdem <ilyadem4@gmail.com>
2023-02-14 22:42:51 +0300

    add hashrouter instead browserrouter

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,16 @@ import App from './App'
 import './index.css'
 import 'animate.css'
 import {QueryClient, QueryClientProvider} from "react-query"
-import {BrowserRouter} from "react-router-dom"
+import {BrowserRouter, HashRouter} from "react-router-dom"
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <QueryClientProvider client={queryClient}>
                 <App/>
             </QueryClientProvider>
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>,
 )
